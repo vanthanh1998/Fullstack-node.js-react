20509112fc0ea97bc2796d30f9fa8d6134a4b58c
Văn Thanh <vanthanh@jv-it.com.vn>
2023-03-06 20:18:23 +0700

    npm cors fix error connect react js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-import connectDB  from "./config/connectDB";
+import connectDB from "./config/connectDB";
+import cors from "cors"
+
 require('dotenv').config();
 
 let app = express()
+// app.use(cors({ origin: true }));
+app.use(cors({ credentials: true, origin: true })); // dcm
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}))
